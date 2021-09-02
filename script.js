882e76157125f230635ca2e8687c9e15aaba2d1a
TjLondon <tl2677@nyu.edu>
2021-09-02 17:39:55 -0400

    tajana's poem

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ const poems = [
     poem: "Roses are red \n Violets are blue \n Jerry Seinfeld is considering \n Making Bee Movie 2",
     image: "bee.jpeg",
   },
+  {
+    author: "Tajana Rose",
+    poem: "Mangos are orange \n Grapes are blue \n Jek is considering \n running a marathon, you can too!!",
+    image: "jek.jpeg",
+  },
 
   // YOUR CODE HERE: Add an object containing two key value pairs: author and poem. Set the author property as equal to a string with your first name. Set the poem property as equal to a short "roses are red" poem. Use the new line character as in the example when you would like a line break.
 ];
